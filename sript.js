1efd4bcb44ba78b9f497fffc6368886394c66373
Andrean Gwen <rutherford@MacBook-Air-Andrean.local>
2025-02-14 08:24:34 +0700

    add javas

--- sript.js
+++ sript.js
@@ -1,2 +1,29 @@
-const inputBox = document.getElementById("input-box");
-const inputBox = document.getElementById("input-box");
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addTask() {
+    if (inputbox.value === '') {
+        alert ("Kamu Harus Menulis Sesuatu!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement ("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+}
+
+listcontainer.addEventListener ("click", function(e) {
+    if(e.target.tagName === "LI") {
+        e.target.classList.toggle("cek");
+
+    } else if (e.target.tagName === "SPAN") {
+            e.target.parentElement.remove();
+        }
+    }, false);
+
+    function saveData() {
+        localStorage
+    }
