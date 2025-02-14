87bba450cf39ddeebc31315db8eabf0b444cc880
Andrean Gwen <rutherford@MacBook-Air-Andrean.local>
2025-02-14 19:53:21 +0700

    andrean

--- sript.js
+++ sript.js
@@ -1,23 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var yourname = prompt('Sebutkan nama mu😊');
-    if (yourname) { // Pastikan user mengisi nama
+    var yourname = localStorage.getItem("username");
+
+    if (!yourname) { 
+        yourname = prompt('Sebutkan nama mu😊');
+        if (yourname) { 
+            localStorage.setItem("username", yourname);
+        }
+    }
+
+    if (yourname) { 
         var message = 'Halo ' + yourname;
         document.getElementById('ucapan').innerText = message;
     }
 });
-
 
 const inputbox = document.getElementById("input-box");
 const listcontainer = document.getElementById("list-container");
 
 function addTask() {
     if (inputbox.value === '') {
-        alert ("Kamu Harus Menulis Sesuatu!");
+        alert("Kamu Harus Menulis Sesuatu!");
     } else {
         let li = document.createElement("li");
         li.innerHTML = inputbox.value;
         listcontainer.appendChild(li);
-        let span = document.createElement ("span");
+        let span = document.createElement("span");
         span.innerHTML = "\u00d7";
         li.appendChild(span);
     }
@@ -25,22 +32,21 @@ function addTask() {
     saveData();
 }
 
-listcontainer.addEventListener ("click", function(e) {
-    if(e.target.tagName === "LI") {
+listcontainer.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("cek");
         saveData();
-
     } else if (e.target.tagName === "SPAN") {
-            e.target.parentElement.remove();
-            saveData();
-        }
-    }, false);
-
-    function saveData() {
-        localStorage.setItem("data", listcontainer.innerHTML);
+        e.target.parentElement.remove();
+        saveData();
     }
+}, false);
 
-    function showTask() {
-        listcontainer.innerHTML = localStorage.getItem("data");
-    }
-    showTask();
+function saveData() {
+    localStorage.setItem("data", listcontainer.innerHTML);
+}
+
+function showTask() {
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
